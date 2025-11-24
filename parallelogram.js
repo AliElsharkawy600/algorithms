89d71ea1729412f7b4bdee2e4db251fe528a0f92
Ali Elsharkawy <as4562373@gmail.com>
2025-11-24 16:14:47 +0200

    calculate the area of Paralleelogram and trapezoid

---
+++ parallelogram.js
@@ -0,0 +1,7 @@
+function ParallelogramAlgo(b, h){
+    return b*h;
+}
+
+
+console.log(ParallelogramAlgo(2,3))
+console.log(ParallelogramAlgo(5,6))
---
+++ trapezoid.js
@@ -0,0 +1,6 @@
+function ParallelogramAlgo(a, b, h) {
+  return 0.5 * (a + b) * h;
+}
+
+console.log(ParallelogramAlgo(2,2, 3));
+console.log(ParallelogramAlgo(3,5, 6));
